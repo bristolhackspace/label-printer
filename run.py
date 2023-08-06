eb6922d771911da20d3102d0dc96a5ab434cc0f2
Sam <633856+SamP20@users.noreply.github.com>
2023-08-06 19:41:31 +0100

    GPIO debouncing

--- run.py
+++ run.py
@@ -1,27 +1,39 @@
 import label_print
 import RPi.GPIO as GPIO
 import time
-import sys
 
-SHORT_STAY_BUTTON = 17
-PROJECT_BOX_BUTTON = 27
+PROJECT_BOX_BUTTON = 23
+SHORT_STAY_BUTTON = 24
 
-def print_short_stay_label(channel):
-    label_print.print_short_stay_label()
+DEBOUNCE_TIME = 0.2
 
-def print_project_box_label(channel):
-    label_print.print_project_box_label()
+def check_channel(channel, callback):
+    if GPIO.input(channel):
+        start = time.monotonic()
+        # Wait long enough for pin to be stable
+        while time.monotonic() - start > DEBOUNCE_TIME:
+            if not GPIO.input(channel):
+                return
+        
+        callback()
+
+        # Limit maximum button push rate
+        time.sleep(0.5)
+
+        # Wait for button to be released before finishing
+        while GPIO.input(channel):
+            time.sleep(0.1)
+
 
 def main():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SHORT_STAY_BUTTON, GPIO.IN, GPIO.PUD_DOWN)
-    GPIO.setup(PROJECT_BOX_BUTTON, GPIO.IN, GPIO.PUD_DOWN)
-
-    GPIO.add_event_detect(SHORT_STAY_BUTTON, GPIO.RISING, callback=print_short_stay_label, bouncetime=200)
-    GPIO.add_event_detect(PROJECT_BOX_BUTTON, GPIO.RISING, callback=print_project_box_label, bouncetime=200)
+    GPIO.setup(PROJECT_BOX_BUTTON, GPIO.IN)
+    GPIO.setup(SHORT_STAY_BUTTON, GPIO.IN)
 
     while True:
-        time.sleep(0.5)
+        time.sleep(0.1)
+        check_channel(PROJECT_BOX_BUTTON, label_print.print_project_box_label)
+        check_channel(SHORT_STAY_BUTTON, label_print.print_short_stay_label)
 
 
 if __name__ == "__main__":
